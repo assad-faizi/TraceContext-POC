14f4e8e4f2bb3ef758b5d7a8e2d79542d02cb4c8
Assad Faizi <assad.faizi@maersk.com>
2022-04-01 10:38:43 +0100

    Basic Tracing Setup with Jaeger

---
+++ src/Client.WebApi/Controllers/OrdersController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Client.WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class OrdersController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok();
+        }
+    }
+}
---
+++ src/Client.WebApi/Program.cs
@@ -0,0 +1,43 @@
+using OpenTelemetry;
+using OpenTelemetry.Resources;
+using OpenTelemetry.Trace;
+
+var builder = WebApplication.CreateBuilder(args);
+var config = builder.Configuration;
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.AddOpenTelemetryTracing(builder =>
+{
+    builder.AddAspNetCoreInstrumentation()
+        .AddHttpClientInstrumentation()
+        .AddSource("Client.WebApi")
+        .SetResourceBuilder(ResourceBuilder.CreateDefault().AddService("Client.WebApi"))
+        .AddJaegerExporter(opts =>
+        {
+            opts.AgentHost = config["Jaeger:AgentHost"];
+            opts.AgentPort = Convert.ToInt32(config["Jaeger:AgentPort"]);
+            opts.ExportProcessorType = ExportProcessorType.Simple;
+        });
+});
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
